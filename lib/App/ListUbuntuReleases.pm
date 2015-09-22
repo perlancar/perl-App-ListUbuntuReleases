0f467e3fa34a1faf3ca94be2a8cb1539920d8b76
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-22 19:17:51 +0700

    Initial commit

---
+++ lib/App/ListUbuntuReleases.pm
@@ -0,0 +1,13 @@
+package App::ListDebianReleases;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: List Debian releases (CLI)
+
+=head1 DESCRIPTION
+
+See the included script L<list-debian-releases>.
+
+=cut
